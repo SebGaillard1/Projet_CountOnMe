51295f0e2285736e8c9898fce9d05e4b74111f7f
Sebastien Gaillard <gaillard.sebastien@outlook.fr>
2021-06-04 11:28:58 +0200

    Added MVC

--- CountOnMe/Controller/ViewController.swift
+++ CountOnMe/Controller/ViewController.swift
@@ -87,26 +87,9 @@ class ViewController: UIViewController {
         }
         
         // Create local copy of operations
-        var operationsToReduce = elements
+        var brain = Brain(operations: elements)
         
-        // Iterate over operations while an operand still here
-        while operationsToReduce.count > 1 {
-            let left = Int(operationsToReduce[0])!
-            let operand = operationsToReduce[1]
-            let right = Int(operationsToReduce[2])!
-            
-            let result: Int
-            switch operand {
-            case "+": result = left + right
-            case "-": result = left - right
-            default: fatalError("Unknown operator !")
-            }
-            
-            operationsToReduce = Array(operationsToReduce.dropFirst(3))
-            operationsToReduce.insert("\(result)", at: 0)
-        }
-        
-        textView.text.append(" = \(operationsToReduce.first!)")
+        textView.text.append(" = \(brain.getResult())")
     }
 
 }
--- CountOnMe/Model/Brain.swift
+++ CountOnMe/Model/Brain.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+
+class Brain {
+    
+    private var operationsToReduce: [String]
+  
+    init(operations: [String]) {
+        operationsToReduce = operations
+        performOperation()
+    }
+    
+    private func performOperation() {
+        // Iterate over operations while an operand still here
+            while operationsToReduce.count > 1 {
+                let left = Int(operationsToReduce[0])!
+                let operand = operationsToReduce[1]
+                let right = Int(operationsToReduce[2])!
+                
+                let result: Int
+                switch operand {
+                case "+": result = left + right
+                case "-": result = left - right
+                default: fatalError("Unknown operator !")
+                }
+                
+                operationsToReduce = Array(operationsToReduce.dropFirst(3))
+                operationsToReduce.insert("\(result)", at: 0)
+            }
+    }
+    
+    func getResult() -> String {
+        return operationsToReduce.first!
+    }
+}
